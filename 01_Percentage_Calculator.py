d85b631ce8c164c727c33faa5a972933fcf7a033
Mustafa786-Python <mustafachamran786313@gmail.com>
2026-02-18 23:09:36 +0500

    Refactor UI elements in Percentage Calculator for consistent styling and layout

--- 01_Percentage_Calculator.py
+++ 01_Percentage_Calculator.py
@@ -8,62 +8,73 @@ from tkinter import ttk, messagebox
 root = tk.Tk()
 root.title("Percentage Calulator")
 root.geometry("600x600")
+root.configure(bg="#333333")
 
+"-------------------------  Frame1   -----------------------------"
+frame = tk.Frame(root, bg="#333333")
+frame.grid(row=0, column=0, sticky="nsew",)
 
-"-------------------------  Frame   -----------------------------"
-frame = tk.Frame(root)
-frame.grid(row=0, column=0, sticky="nsew")
+title_label = tk.Label(frame, text="Percentage Calulator",
+                       font=("Arial", 30), bg="#333333", fg="#ffffff")
+title_label.pack()
+
+"-------------------------  Frame1   -----------------------------"
+frame1 = tk.Frame(root, bg="#333333")
+frame1.grid(row=1, column=0, sticky="nsew",)
 
 # name label
-name_label = tk.Label(frame, text=" Name :   ")
+name_label = tk.Label(frame1, text=" Name :   ",  bg="#333333", fg="#ffffff")
 name_label.grid(row=0, column=0, pady=10, padx=10)
 
 # name input
-name_entry = tk.Entry(frame)
+name_entry = tk.Entry(frame1)
 name_entry.grid(row=0, column=1, sticky="ew", columnspan=2, pady=10, padx=10)
 
 # total mark label
-total_label = tk.Label(frame, text="    Total Marks :   ")
+total_label = tk.Label(frame1, text="    Total Marks :   ",
+                       bg="#333333", fg="#ffffff")
 total_label.grid(row=0, column=3, pady=10, padx=10)
 
 # total mark input
-total_entry = tk.Entry(frame)
+total_entry = tk.Entry(frame1)
 total_entry.grid(row=0, column=4, sticky="ew", columnspan=2, pady=10, padx=10)
 
 # Subject Label
-name_label = tk.Label(frame, text=" Subjects :   ")
+name_label = tk.Label(frame1, text=" Subjects :   ",
+                      bg="#333333", fg="#ffffff")
 name_label.grid(row=1, column=0, pady=10, padx=10)
 
 "-----  Subjects labels ----------"
 # Maths
-math_label = tk.Label(frame, text=" Physics  ")
+math_label = tk.Label(frame1, text=" Physics  ",  bg="#333333", fg="#ffffff")
 math_label.grid(row=1, column=1, columnspan=2)
 
 # physics
-physics_label = tk.Label(frame, text=" Maths   ")
+physics_label = tk.Label(frame1, text=" Maths   ",  bg="#333333", fg="#ffffff")
 physics_label.grid(row=1, column=3, columnspan=2)
 
 # Computer
-computer_label = tk.Label(frame, text=" Computer ")
+computer_label = tk.Label(frame1, text=" Computer ",
+                          bg="#333333", fg="#ffffff")
 computer_label.grid(row=1, column=5, columnspan=2)
 
 "-----  Subjects Entries ----------"
 # Maths
-math_entry = tk.Entry(frame)
+math_entry = tk.Entry(frame1)
 math_entry.grid(row=2, column=1, sticky="ew", columnspan=2)
 
 # physics
-physics_entry = tk.Entry(frame)
+physics_entry = tk.Entry(frame1)
 physics_entry.grid(row=2, column=3, sticky="ew", columnspan=2)
 
 # Computer
-computer_entry = tk.Entry(frame)
+computer_entry = tk.Entry(frame1)
 computer_entry.grid(row=2, column=5, sticky="ew", columnspan=2)
 
 
 "---------Creating Table---------"
 columns = ("name", "p%")
-tree = ttk.Treeview(frame, columns=columns, show="headings")
+tree = ttk.Treeview(frame1, columns=columns, show="headings")
 tree.heading("name", text="Name")
 tree.heading("p%", text="Percentage")
 tree.grid(row=3, column=1, columnspan=7, pady=10, sticky="nsew")
@@ -156,11 +167,13 @@ physics_entry.bind("<Tab>", entry_delete)
 computer_entry.bind("<Tab>", entry_delete)
 
 "Add Buttons"
-add_btn = tk.Button(frame, text="ADD", command=add_item)
+add_btn = tk.Button(frame1, text="ADD", command=add_item,
+                    bg="#333333", fg="#ffffff")
 add_btn.grid(row=2, column=7, padx=10)
 
 # delete button
-tk.Button(root, text="Delete Selected", command=delete_item).grid(
+tk.Button(root, text="Delete Selected", command=delete_item,  bg="#333333", fg="#ffffff").grid(
     row=3, column=0, columnspan=4, pady=5)
+
 
 root.mainloop()
