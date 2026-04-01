1ab60ea7711605628e400d3a50b29e4668ee3b95
Mustafa786-Python <mustafachamran786313@gmail.com>
2026-04-01 22:35:17 +0500

    Add To Do List application using CustomTkinter

---
+++ 05_To_do_list_app/todo_app.py
@@ -0,0 +1,30 @@
+import customtkinter as ctk
+
+root = ctk.CTk()
+root.geometry("750x450")
+root.title("To do app")
+
+def add_todo():
+    todo = entry.get()
+    label = ctk.CTkLabel(scrollable_frame, text=todo)
+    label.pack()
+    entry.delete(0, ctk.END)
+    
+     
+
+
+title_label = ctk.CTkLabel(root, text="Daily Tasks",
+                           font=ctk.CTkFont(size=30, weight="bold"))
+title_label.pack(padx=10, pady=(40, 20))
+
+scrollable_frame = ctk.CTkScrollableFrame(root, width=500, height=200)
+scrollable_frame.pack()
+
+entry = ctk.CTkEntry(scrollable_frame, placeholder_text="Add todo")
+entry.pack(fill="x")
+
+button = ctk.CTkButton(root, text="Add", width=500, command=add_todo)
+button.pack(pady=20)
+
+
+root.mainloop()
